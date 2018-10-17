3f90ec0115ffa221280a991219dc91529b2966d1
erin <e.maguire@outlook.com>
2018-10-17 14:12:10 +0100

    updated dispel script

--- alpine-gcloud-helm-deploy/dispel.sh
+++ alpine-gcloud-helm-deploy/dispel.sh
@@ -9,21 +9,11 @@ fi
 APP_NAME=$1
 TARGET_ENV=$2
 
-OFFLINE_COLOUR=$(kubectl get service/$TARGET_ENV-$APP_NAME-offline -o=jsonpath="{.spec.selector.colour}")
-if [[ -z "${OFFLINE_COLOUR}" ]]; then
-  OFFLINE_COLOUR="blue"
-fi
-
-ONLINE_COLOUR=$(kubectl get service/$TARGET_ENV-$APP_NAME -o=jsonpath="{.spec.selector.colour}")
-if [[ -z "${ONLINE_COLOUR}" ]]; then
-  ONLINE_COLOUR="green"
-fi
-
-echo "Dispelling $TARGET_ENV-$OFFLINE_COLOUR-$APP_NAME"
-OFFLINE_DISPEL_RESULT=$(helm delete --purge $TARGET_ENV-$OFFLINE_COLOUR-$APP_NAME 2>&1)
+echo "Dispelling $TARGET_ENV-$APP_NAME"
+OFFLINE_DISPEL_RESULT=$(helm delete --purge $TARGET_ENV-$APP_NAME 2>&1)
 echo "Dispel result: $OFFLINE_DISPEL_RESULT"
 
-if [[ $OFFLINE_DISPEL_RESULT = *"not found"* ]] || [[ $OFFLINE_DISPEL_RESULT = "release \"$TARGET_ENV-$OFFLINE_COLOUR-$APP_NAME\" deleted" ]]; then
+if [[ $OFFLINE_DISPEL_RESULT = *"not found"* ]] || [[ $OFFLINE_DISPEL_RESULT = "release \"$TARGET_ENV-$APP_NAME\" deleted" ]]; then
   echo "Successful dispel!"
 
   echo "Dispelling $TARGET_ENV-service-$APP_NAME"
@@ -33,11 +23,11 @@ if [[ $OFFLINE_DISPEL_RESULT = *"not found"* ]] || [[ $OFFLINE_DISPEL_RESULT = "
   if [[ $SERVICE_DISPEL_RESULT = *"not found"* ]] || [[ $SERVICE_DISPEL_RESULT = "release \"$TARGET_ENV-service-$APP_NAME\" deleted" ]]; then
     echo "Successful dispel!"
 
-    echo "Dispelling $TARGET_ENV-$ONLINE_COLOUR-$APP_NAME"
-    ONLINE_DISPEL_RESULT=$(helm delete --purge $TARGET_ENV-$ONLINE_COLOUR-$APP_NAME 2>&1)
+    echo "Dispelling $TARGET_ENV-$APP_NAME"
+    ONLINE_DISPEL_RESULT=$(helm delete --purge $TARGET_ENV-$APP_NAME 2>&1)
     echo "Dispel result: $ONLINE_DISPEL_RESULT"
 
-    if [[ $ONLINE_DISPEL_RESULT = *"not found"* ]] || [[ $ONLINE_DISPEL_RESULT = "release \"$TARGET_ENV-$ONLINE_COLOUR-$APP_NAME\" deleted" ]]; then
+    if [[ $ONLINE_DISPEL_RESULT = *"not found"* ]] || [[ $ONLINE_DISPEL_RESULT = "release \"$TARGET_ENV-$APP_NAME\" deleted" ]]; then
       echo "Successful dispel!"
     else
       echo "Failed to dispel."
