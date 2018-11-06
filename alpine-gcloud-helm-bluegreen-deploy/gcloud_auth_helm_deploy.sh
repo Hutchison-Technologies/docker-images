528ac1e99eca6162d64205ba642ca6fd5b7a39e2
liam humphreys <liam.humphreys1989@gmail.com>
2018-11-06 15:36:25 +0000

    add gcloud bluegreen deploy script

---
+++ alpine-gcloud-helm-bluegreen-deploy/gcloud_auth_helm_deploy.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 2 ]
+then
+  echo "Usage: gcloud_auth_helm_deploy.sh CHART_DIR APP_NAME TARGET_ENV TARGET_VER"
+  exit 1
+fi
+
+CHART_DIR=$1
+APP_NAME=$2
+TARGET_ENV=$3
+TARGET_VER=$4
+
+if [[ ! -d ${CHART_DIR} ]]; then
+    echo "${CHART_DIR} is not a directory."
+    exit 1
+fi
+
+if [ "${TARGET_ENV}" != "staging" ] && [ "${TARGET_ENV}" != "prod" ] ; then
+    echo "${TARGET_ENV} is not a valid target env."
+    exit 1
+fi
+
+GOOGLE_APPLICATION_CREDENTIALS=/tmp/key.json
+
+if [[ -z "${GOOGLE_SERVICE_KEY_BLOB}" ]]; then
+  echo "GOOGLE_SERVICE_KEY_BLOB was not set."
+  exit 1
+else
+  printf ${GOOGLE_SERVICE_KEY_BLOB} | base64 -d > ${GOOGLE_APPLICATION_CREDENTIALS}
+fi
+
+gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+
+if [[ -z "${PROJECT_ID}" ]]; then
+  echo "PROJECT_ID was not set."
+  exit 1
+else
+  gcloud config set project ${PROJECT_ID}
+fi
+
+if [[ -z "${REGION}" ]]; then
+  echo "REGION was not set."
+  exit 1
+else
+  gcloud config set compute/region ${REGION}
+fi
+
+if [[ -z "${CLUSTER_ID}" ]]; then
+  echo "CLUSTER_ID was not set."
+  exit 1
+else
+  gcloud beta container clusters get-credentials --region ${REGION} ${CLUSTER_ID}
+fi
+
+helm init --wait --service-account tiller --client-only
+
+if [[ -z "${CHARTMUSEUM}" ]]; then
+  echo "CHARTMUSEUM was not set."
+  exit 1
+else
+  helm repo add chartmuseum ${CHARTMUSEUM}
+fi
+
+helm dep update ${CHART_DIR} 
+helm dep build ${CHART_DIR}
+
+if ! helm lint --strict -f ${CHART_DIR}; then
+  echo "${CHART_DIR} contains malformed chart."
+  exit 1
+fi
+
+/deploy.sh ${CHART_DIR} ${APP_NAME} ${TARGET_ENV} ${TARGET_VER}
+
+gcloud auth revoke --all
+rm $GOOGLE_APPLICATION_CREDENTIALS
