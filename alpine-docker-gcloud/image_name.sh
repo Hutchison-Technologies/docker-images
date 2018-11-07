ee4a46593ac54d9937702f8e16f842c6025a385a
liam humphreys <liam.humphreys1989@gmail.com>
2018-11-07 16:25:57 +0000

    fix image name script to handle setup images

--- alpine-docker-gcloud/image_name.sh
+++ alpine-docker-gcloud/image_name.sh
@@ -9,16 +9,22 @@ fi
 TARGET_ENV=$1
 APP_NAME=$2
 
-ONLINE_COLOUR=$(kubectl get service/$TARGET_ENV-$APP_NAME -o=jsonpath="{.spec.selector.colour}")
+SETUP_SUFFIX="-setup"
+APP_NAME_SANS_SUFFIX=${APP_NAME%"$SETUP_SUFFIX"}
+
+ONLINE_COLOUR=$(kubectl get service/${TARGET_ENV}-${APP_NAME_SANS_SUFFIX} -o=jsonpath="{.spec.selector.colour}")
 if [[ -z "${ONLINE_COLOUR}" ]]; then
-    echo "kubectl get service/$TARGET_ENV-$APP_NAME -o=jsonpath=\"{.spec.selector.colour}\" - Returned no colour"
+    echo "kubectl get service/${TARGET_ENV}-${APP_NAME_SANS_SUFFIX} -o=jsonpath=\"{.spec.selector.colour}\" - Returned no colour"
     exit 1
 fi
 
-ONLINE_IMAGE=$(kubectl get deployment/$TARGET_ENV-$ONLINE_COLOUR-$APP_NAME -o=jsonpath="{.spec.template.spec.containers[0].image}")
-if [[ -z "${ONLINE_IMAGE}" ]]; then
-    echo "kubectl get deployment/$TARGET_ENV-$ONLINE_COLOUR-$APP_NAME -o=jsonpath=\"{.spec.template.spec.containers[0].image}\" - Returned no image"
+ONLINE_DEPLOYMENT_IMAGE=$(kubectl get --ignore-not-found deployment/${TARGET_ENV}-${ONLINE_COLOUR}-${APP_NAME} -o=jsonpath="{.spec.template.spec.containers[0].image}")
+ONLINE_JOB_IMAGE=$(kubectl get --ignore-not-found job/${TARGET_ENV}-${ONLINE_COLOUR}-${APP_NAME} -o=jsonpath="{.spec.template.spec.containers[0].image}")
+
+if [[ -z "${ONLINE_DEPLOYMENT_IMAGE}" ]] && [[ -z "${ONLINE_JOB_IMAGE}" ]]; then
+    echo "Cannot find live image for ${TARGET_ENV} ${APP_NAME}."
     exit 1
 fi
 
-echo $ONLINE_IMAGE
+FOUND_IMAGE="${ONLINE_DEPLOYMENT_IMAGE}" && [[ -z "${ONLINE_DEPLOYMENT_IMAGE}" ]] && FOUND_IMAGE="${ONLINE_JOB_IMAGE}"
+echo $FOUND_IMAGE
