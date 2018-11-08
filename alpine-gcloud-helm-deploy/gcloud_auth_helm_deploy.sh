3990e6b8088710e975c5aac0d8dbb99db50b4f12
liam humphreys <liam.humphreys1989@gmail.com>
2018-11-08 11:09:23 +0000

    allow deploy script to have version

--- alpine-gcloud-helm-deploy/gcloud_auth_helm_deploy.sh
+++ alpine-gcloud-helm-deploy/gcloud_auth_helm_deploy.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 3 ]
+if [ "$#" -lt 3 ] || [ "$#" -gt 4 ]
 then
-  echo "Usage: gcloud_auth_helm_deploy.sh CHART_DIR APP_NAME TARGET_ENV"
+  echo "Usage: gcloud_auth_helm_deploy.sh CHART_DIR APP_NAME TARGET_ENV {OPTIONAL:TARGET_VER}"
   exit 1
 fi
 
 CHART_DIR=$1
 APP_NAME=$2
 TARGET_ENV=$3
+TARGET_VER=$4
 
 if [[ ! -d ${CHART_DIR} ]]; then
     echo "${CHART_DIR} is not a directory."
@@ -69,21 +70,26 @@ if ! helm lint -f ${CHART_DIR}/${TARGET_ENV}.yaml ${CHART_DIR}; then
   exit 1
 fi
 
-echo "Deploying from ${CHART_DIR} to: ${TARGET_ENV}-${APP_NAME}"
-if helm upgrade ${TARGET_ENV}-${APP_NAME} ${CHART_DIR} -f ${CHART_DIR}/${TARGET_ENV}.yaml --install --force --recreate-pods --wait --timeout=120; then
-    echo "Successfully upgraded"
+if [[ -z "${TARGET_VER}" ]]; then
+  echo "NON-VERSIONED DEPLOY!"
+  echo "Deploying from ${CHART_DIR} to: ${TARGET_ENV}-${APP_NAME}"
+  if helm upgrade ${TARGET_ENV}-${APP_NAME} ${CHART_DIR} -f ${CHART_DIR}/${TARGET_ENV}.yaml --install --force --recreate-pods --wait --timeout=120; then
+      echo "Successfully upgraded"
+  else
+      LAST_GOOD_REV=$(helm history ${TARGET_ENV}-${APP_NAME} -o json | jq -r -M --arg DEPLOYED "DEPLOYED" '[.[] | select(.status==$DEPLOYED)] | reverse | .[0] | .revision')
+      
+      int_reg='^[0-9]+$'
+      if ! [[ ${LAST_GOOD_REV} =~ $int_reg ]] ; then
+          echo "Failed upgrade, rolling back to 0"
+          helm rollback --force --recreate-pods --wait --timeout=600 ${TARGET_ENV}-${APP_NAME} 0
+      else
+          echo "Failed upgrade, rolling back to ${LAST_GOOD_REV}"
+          helm rollback --force --recreate-pods --wait --timeout=600 ${TARGET_ENV}-${APP_NAME} ${LAST_GOOD_REV}
+      fi
+      exit 1
+  fi
 else
-    LAST_GOOD_REV=$(helm history ${TARGET_ENV}-${APP_NAME} -o json | jq -r -M --arg DEPLOYED "DEPLOYED" '[.[] | select(.status==$DEPLOYED)] | reverse | .[0] | .revision')
-    
-    int_reg='^[0-9]+$'
-    if ! [[ ${LAST_GOOD_REV} =~ $int_reg ]] ; then
-        echo "Failed upgrade, rolling back to 0"
-        helm rollback --force --recreate-pods --wait --timeout=600 ${TARGET_ENV}-${APP_NAME} 0
-    else
-        echo "Failed upgrade, rolling back to ${LAST_GOOD_REV}"
-        helm rollback --force --recreate-pods --wait --timeout=600 ${TARGET_ENV}-${APP_NAME} ${LAST_GOOD_REV}
-    fi
-    exit 1
+  /deploy.sh ${CHART_DIR} ${APP_NAME} ${TARGET_ENV} ${TARGET_VER}
 fi
 
 gcloud auth revoke --all
